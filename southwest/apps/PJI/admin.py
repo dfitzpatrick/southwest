cb076b052e880bb4a873c3fcf1678ba1c2381427
vagrant <vagrant@heroku.isengard.ejholmes.net>
2014-08-18 02:17:57 +0200

    fixed method call admin.site.register in PJI

--- southwest/apps/PJI/admin.py
+++ southwest/apps/PJI/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Franchise, Store
 # Register your models here.
-admin.register(Franchise)
-admin.register(Store)
+admin.site.register(Franchise)
+admin.site.register(Store)
